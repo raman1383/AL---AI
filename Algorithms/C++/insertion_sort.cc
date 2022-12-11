dedf9029ecab2f7ebb877e350476b730e0466788
raman1383 <raman83shariati@gmail.com>
2022-12-12 00:39:08 +0330

    r

--- Algorithms/C++/insertion_sort.cc
+++ Algorithms/C++/insertion_sort.cc
@@ -4,10 +4,12 @@ using namespace std;
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
+
     for (i = 1; i < n; i++)
     {
         key = arr[i];
         j = i - 1;
+
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
--- Algorithms/python/insertionSort.py
+++ Algorithms/python/insertionSort.py
@@ -1,17 +1,14 @@
 def insertionSort(arr):
-
     for i in range(1, len(arr)):
         key = arr[i]
         j = i-1
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+            arr[j+1] = arr[j]
             j -= 1
-        arr[j + 1] = key
-
-    # print
-    for i in range(len(arr)):
-        print("% d" % arr[i])
+        arr[j+1] = key
 
 
 arr = [10, 11, 13, 5, 6, 3, 42]
 insertionSort(arr)
+for i in range(len(arr)):
+    print("% d" % arr[i])
